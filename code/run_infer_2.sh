63cea556f8b6b42e13304c2dc90e62cab655b950
bwalker1 <bwalker@unc.edu>
2018-03-13 13:53:17 -0400

    added caching of config maybe

--- code/run_infer_2.sh
+++ code/run_infer_2.sh
@@ -3,11 +3,18 @@
 # download the model and weights if not present
 wtdir=/root/weights
 
-if [ ${wtdir}/model.pkl ]; then
+if [ -f ${wtdir}/model.pkl ]; then
     echo "Model exists"
 else
     echo "Model doesn't exist, downloading"
     wget -O ${wtdir}/model.pkl https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl 
+fi
+
+if [ -f ${wtdir}/cfg.yaml ]; then
+    echo "Config exists"
+else
+    echo "Config doesn't exist, downloading"
+    wget -O ${wtdir}/cfg.yaml https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml
 fi
 
 
@@ -21,7 +28,7 @@ cp $1 /tmp/docker_mount/images/
 
 
 nvidia-docker run -it -v /tmp/docker_mount:/mnt -v ${wtdir}:/mnt2 detectron python2 tools/infer_simple.py \
-    --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+    --cfg /mnt2/cfg.yaml \
     --output-dir /mnt/detectron-visualizations \
     --image-ext jpg \
     --wts /mnt2/model.pkl \
