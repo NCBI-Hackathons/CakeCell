30734c1bf39116fbb2770d5b0f66f14656716c90
bwalker1 <bwalker@unc.edu>
2018-03-13 12:26:36 -0400

    put in the weight/cfg variables

--- code/run_infer.sh
+++ code/run_infer.sh
@@ -10,10 +10,10 @@ cfg=https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_
 wts=https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_2x.yaml.02_00_30.ESWbND2w/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl
 
 nvidia-docker run -it -v /tmp/docker_mount:/mnt  detectron python2 tools/infer_simple.py \
-    --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+    --cfg $cfg \
     --output-dir /mnt/detectron-visualizations \
     --image-ext jpg \
-    --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
+    --wts $wts \
     /mnt/images
     
 
@@ -23,3 +23,12 @@ cp -r /tmp/docker_mount/* ${PWD}/results
 
 # get rid of mount folder
 rm -rf /tmp/docker_mount
+
+
+# old command
+#nvidia-docker run -it -v /tmp/docker_mount:/mnt  detectron python2 tools/infer_simple.py \
+#    --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+#    --output-dir /mnt/detectron-visualizations \
+#    --image-ext jpg \
+#    --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
+#    /mnt/images
