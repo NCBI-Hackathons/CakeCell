857f8158e1102a6ee974e1f2abf4c9c21c886af1
bwalker1 <bwalker@unc.edu>
2018-03-13 13:23:06 -0400

    for real

--- code/run_infer.sh
+++ code/run_infer.sh
@@ -6,15 +6,16 @@ mkdir -p /tmp/docker_mount/images
 # copy input file to mount
 cp $1 /tmp/docker_mount/images/
 
-cfg="https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_2x.yaml"
-wts="https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_2x.yaml.02_00_30.ESWbND2w/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl"
+#cfg="https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_2x.yaml"
+#wts="https://s3-us-west-2.amazonaws.com/detectron/35859745/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_2x.yaml.02_00_30.ESWbND2w/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl"
 
 nvidia-docker run -it -v /tmp/docker_mount:/mnt  detectron python2 tools/infer_simple.py \
-    --cfg $cfg \
+    --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
     --output-dir /mnt/detectron-visualizations \
     --image-ext jpg \
-    --wts $wts \
+    --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
     /mnt/images
+
     
 
 # copy output to permanent folder
@@ -26,9 +27,9 @@ rm -rf /tmp/docker_mount
 
 
 # old command
-#nvidia-docker run -it -v /tmp/docker_mount:/mnt  detectron python2 tools/infer_simple.py \
-#    --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
-#    --output-dir /mnt/detectron-visualizations \
-#    --image-ext jpg \
-#    --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
-#    /mnt/images
+nvidia-docker run -it -v /tmp/docker_mount:/mnt  detectron python2 tools/infer_simple.py \
+    --cfg $cfg \
+    --output-dir /mnt/detectron-visualizations \
+    --image-ext jpg \
+    --wts $wts \
+    /mnt/images
