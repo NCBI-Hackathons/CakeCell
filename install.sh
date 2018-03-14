40ac8530baad95dd690ec32605bcba9ef22a2c58
bwalker1 <bwalker@unc.edu>
2018-03-14 11:41:24 -0400

    fixed boolean error

--- install.sh
+++ install.sh
@@ -6,80 +6,82 @@ TESLADRIVERLOC="http://us.download.nvidia.com/tesla/390.30/nvidia-diag-driver-lo
 
 # TODO: clean up after ourselves
 
-if [ -e /install ]
+if [ ! -e /install ]
 then
-# begin and install drivers
-mkdir /install
-cd install
+    # begin and install drivers
+    mkdir /install
+    cd install
 
-# do we need to update anything?
-yum update
-yum install git
-yum install wget
+    # do we need to update anything?
+    yum update
+    yum install git
+    yum install wget
 
 
 
-# Install CUDA drivers
+    # Install CUDA drivers
 
-# See if we can find the right one automatically
-# TODO: create override
-echo "Beginning driver installation"
-wget -O driver.rpm $TESLADRIVERLOC
-# check that this is correct
-yum install -y driver.rpm
-yum clean all
-yum install -y cuda_drivers
-echo "Completed driver installation"
+    # See if we can find the right one automatically
+    # TODO: create override
+    echo "Beginning driver installation"
+    wget -O driver.rpm $TESLADRIVERLOC
+    # check that this is correct
+    yum install -y driver.rpm
+    yum clean all
+    yum install -y cuda_drivers
+    echo "Completed driver installation"
+    
+    reboot
 
 else
-# do rest of installation
+    # do rest of installation
 
-# Install CUDA
-echo "Beginning CUDA installation"
+    # Install CUDA
+    echo "Beginning CUDA installation"
 
-wget -O cuda_install.rpm $CUDALOC
-rpm -i cuda_install.rpm
-yum clean all
-yum install -y cuda
+    wget -O cuda_install.rpm $CUDALOC
+    rpm -i cuda_install.rpm
+    yum clean all
+    yum install -y cuda
 
-echo "Completed CUDA installation"
+    echo "Completed CUDA installation"
 
 
 
-echo "Beginning docker installation"
-# Install docker-ce
-yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+    echo "Beginning docker installation"
+    # Install docker-ce
+    yum install -y yum-utils \
+      device-mapper-persistent-data \
+      lvm2
   
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+    yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
 
 
-yum install -y docker-ce-17.12.1.ce
+    yum install -y docker-ce-17.12.1.ce
 
 
-# Install nvidia-docker
-curl -s -L https://nvidia.github.io/nvidia-docker/centos7/x86_64/nvidia-docker.repo | \
-  sudo tee /etc/yum.repos.d/nvidia-docker.repo
-yum install -y nvidia-docker2-2.0.3-1.docker17.12.1.ce
-systemctl start docker
-echo "Completed docker installation"
+    # Install nvidia-docker
+    curl -s -L https://nvidia.github.io/nvidia-docker/centos7/x86_64/nvidia-docker.repo | \
+      sudo tee /etc/yum.repos.d/nvidia-docker.repo
+    yum install -y nvidia-docker2-2.0.3-1.docker17.12.1.ce
+    systemctl start docker
+    echo "Completed docker installation"
 
 
-# Download detectron
-git clone https://github.com/facebookresearch/Detectron.git /detectron
-# build docker file
-cd /detectron/docker
-docker build -t detectron .
+    # Download detectron
+    git clone https://github.com/facebookresearch/Detectron.git /detectron
+    # build docker file
+    cd /detectron/docker
+    docker build -t detectron .
 
 
-# TODO: Set up our wrapper stuff to make it more convenient to run
+    # TODO: Set up our wrapper stuff to make it more convenient to run
 
-# Set up stuff to run
+    # Set up stuff to run
 
-echo "Starting docker"
-systemctl start docker
+    echo "Starting docker"
+    systemctl start docker
 
 fi
