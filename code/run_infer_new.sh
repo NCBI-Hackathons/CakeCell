2a9b9cc02ba658761abd427042a2dd430e12d5da
Ben Walker <walkeb6@live.unc.edu>
2018-03-14 19:59:01 +0000

    small infer changes

--- code/run_infer_new.sh
+++ code/run_infer_new.sh
@@ -1,18 +1,19 @@
 wtname=`python2 model_cache.py e2e_mask_rcnn_R-101-FPN_2x`
+wtname=${wtname/shared_cache/mnt2}
 cfgname="/cakecell/models/e2e_mask_rcnn_R-101-FPN_2x.yaml"
-
+imdir="/cakecell/images"
 # create the communications folder
 rm -rf /tmp/docker_mount
 mkdir -p /tmp/docker_mount
 mkdir -p /tmp/docker_mount/images
 
 # copy input file to mount
-cp ${imdir}/* /tmp/docker_mount/images/
+cp ${imdir}/*.jpg /tmp/docker_mount/images/
 
-inferscript=/cakecell/code/infer0.py
+inferscript=tools/infer_simple.py
 
 
-nvidia-docker run -it -v /tmp/docker_mount:/mnt -v ${wtdir}:/mnt2 detectron python2 $inferscript \
+nvidia-docker run -it -v /tmp/docker_mount:/mnt -v /shared_cache:/mnt2 detectron python2 $inferscript \
     --cfg /mnt2/${cfgname} \
     --output-dir /mnt/detectron-visualizations \
     --image-ext jpg \
