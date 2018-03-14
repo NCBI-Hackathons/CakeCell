3c5f67496f9eddd519099adb7b09b6a9ac000e36
bwalker1 <bwalker@unc.edu>
2018-03-14 16:18:32 -0400

    changed folder of docker_mount

--- code/run_infer_new.sh
+++ code/run_infer_new.sh
@@ -4,8 +4,8 @@ cfgname="/cakecell/models/e2e_mask_rcnn_R-101-FPN_2x.yaml"
 imdir="/cakecell/images"
 # create the communications folder
 rm -rf /tmp/docker_mount
-mkdir -p /tmp/docker_mount
-mkdir -p /tmp/docker_mount/images
+mkdir -p ${PWD}/docker_mount
+mkdir -p ${PWD}/docker_mount/images
 
 # copy input file to mount
 cp ${imdir}/*.jpg /tmp/docker_mount/images/
@@ -13,7 +13,7 @@ cp ${imdir}/*.jpg /tmp/docker_mount/images/
 inferscript=tools/infer_simple.py
 
 
-nvidia-docker run -it -v /tmp/docker_mount:/mnt -v /shared_cache:/mnt2 detectron python2 $inferscript \
+nvidia-docker run -it -v ${PWD}/docker_mount:/mnt -v /shared_cache:/mnt2 detectron python2 $inferscript \
     --cfg /mnt2/${cfgname} \
     --output-dir /mnt/detectron-visualizations \
     --image-ext jpg \
@@ -24,6 +24,6 @@ nvidia-docker run -it -v /tmp/docker_mount:/mnt -v /shared_cache:/mnt2 detectron
 # copy output to permanent folder
 #mkdir -p ${PWD}/results
 #cp -r /tmp/docker_mount/detectron-visualizations/* ${PWD}/results
-gsutil cp -r /tmp/docker_mount/detectron-visualizations gs://gems-drive/
+gsutil cp -r ${PWD}/docker_mount/detectron-visualizations gs://gems-drive/
 # get rid of mount folder
-rm -rf /tmp/docker_mount
+rm -rf ${PWD}/docker_mount
